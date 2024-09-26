871831db605ee9d7125c084e4996c4d861306c0a
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-09-26 17:16:49 +0100

    Add game model

--- gameblog/models.py
+++ gameblog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    company = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    platform = models.CharField(max_length=50)  # e.g., PC, PlayStation, Xbox
+    created_at = models.DateTimeField(auto_now_add=True)  # Auto timestamp for when the game is added
+    updated_at = models.DateTimeField(auto_now=True)      # Auto timestamp for last modification
+
+    def __str__(self):
+        return self.title
--- gameglance/settings.py
+++ gameglance/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&79%!9dr!tyifg**ung(b(gfp3m!l(j*a+rzi4@24=&e_yqn4&'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'localhost']
