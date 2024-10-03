f032695335bcbf4bb09d7eca9c9b4286a5c078de
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-10-03 18:28:41 +0100

    Add content to game review template

--- gameblog/views.py
+++ gameblog/views.py
@@ -9,3 +9,14 @@ class GameList(generic.ListView):
     queryset = Game.objects.order_by('-created_on')
     template_name = "gameblog/index.html"
     paginate_by = 4
+    
+def game_detail(request, slug):
+    queryset = Game.objects.order_by('-created_on')
+    game = get_object_or_404(queryset, slug=slug)
+    
+    return render(
+        request,
+        "gameblog/game_review.html",
+        {'game': game}
+    )
+
