2536a767e41aa7028cc4e1b37d18f6db8d71900b
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-10-07 12:41:43 +0100

    Add content to about template

--- about/views.py
+++ about/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from .models import About
 
 def about_view(request):
-    return render(request, 'profile.html')
+    return render(request, 'about.html')
--- gameglance/settings.py
+++ gameglance/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'gameblog',
+    'about',
     'user_profile',
     'django_summernote',
     'crispy_forms',
