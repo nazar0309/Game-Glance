27f7a0198c730899b6a6492e0f95f00a578724a2
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-10-08 20:02:15 +0100

    Change view for collaborate form

--- about/views.py
+++ about/views.py
@@ -1,7 +1,25 @@
-# user_profile/views.py
-
 from django.shortcuts import render
-from .models import About
+from .forms import CollaborateForm
+from django.contrib import messages
 
 def about_view(request):
-    return render(request, 'about.html')
+
+    if request.method == "POST":
+        print('Getting the POST request...')
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+            
+    collaborate_form = CollaborateForm()
+    print('Rendering template...')
+    return render(
+        request,
+        "about/about.html",
+         {
+        "collaborate_form": collaborate_form,
+        },
+    )
