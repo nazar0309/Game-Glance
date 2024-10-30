768d6731e3ab6f7fd87ee711f912d6447f39cd36
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-10-30 13:19:04 +0000

    Change the photos of python testing

--- about/models.py
+++ about/models.py
@@ -4,6 +4,7 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+
 class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
